24ba77dc2034bd2b7fab995448ab6292f079110e
Иван <foxneversleep@gmail.com>
2026-08-14 21:43:51 +0300

    Update engine.js

--- game/engine.js
+++ game/engine.js
@@ -1,1 +1,23 @@
+const gameState = {
+    turn: 1,
 
+    activePlayer: "player",
+
+    player: {
+        hp: 30,
+        mana: 1,
+        maxMana: 1,
+        deck: [],
+        hand: [],
+        board: []
+    },
+
+    opponent: {
+        hp: 30,
+        mana: 1,
+        maxMana: 1,
+        deck: [],
+        hand: [],
+        board: []
+    }
+};
