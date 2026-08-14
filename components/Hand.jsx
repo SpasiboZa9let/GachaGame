ef7516d5fb0f1a56452267ff27de5a516c55e653
Иван <foxneversleep@gmail.com>
2026-08-14 23:20:59 +0300

    Update Hand.jsx

--- components/Hand.jsx
+++ components/Hand.jsx
@@ -1,51 +1,13 @@
 function Hand({ cards, onCardClick }) {
 
-    if (!cards) {
+    if (!cards || cards.length === 0) {
         return null;
     }
 
-    return (
-        <div
-            style={{
-                width: "100%",
-
-                display: "flex",
-                flexDirection: "row",
-function Hand({ cards, onCardClick }) {
-
-    if (!cards) {
-        return null;
-    }
 
     return (
 
-        <div
-            style={{
-                width: "100%",
-
-                display: "flex",
-
-                flexDirection: "row",
-
-                justifyContent: "center",
-
-                alignItems: "flex-end",
-
-                padding: "30px 20px 15px",
-
-                boxSizing: "border-box",
-
-                overflowX: "auto",
-
-                minHeight: "300px",
-
-                background:
-                    "linear-gradient(#181818, #101010)",
-
-                borderTop:
-                    "1px solid #333"
-            }}
-        >
+        <div style={styles.hand}>
 
             {cards.map((card, index) => {
 
@@ -55,71 +17,49 @@ function Hand({ cards, onCardClick }) {
 
                 return (
 
-                    <div
-                        key={card.id || index}
-                        style={{
-                            marginLeft:
-                                index === 0
-                                    ? "0"
-                                    : "-45px",
-
-                            zIndex: index,
-
-                            transition:
-                                "margin 0.15s ease"
-                        }}
-                    >
-
-                        <Card
-                            card={card}
-                            onClick={onCardClick}
-                        />
-
-                    </div>
-
-                );
-
-            })}
-
-        </div>
-
-    );
-}
-                flexWrap: "nowrap",
-
-                justifyContent: "center",
-                alignItems: "flex-start",
-
-                gap: "15px",
-
-                padding: "20px",
-
-                boxSizing: "border-box",
-
-                overflowX: "auto",
-
-                background: "#151515",
-
-                border: "2px solid yellow"
-            }}
-        >
-
-            {cards.map((card, index) => {
-
-                if (!card) {
-                    return null;
-                }
-
-                return (
                     <Card
                         key={card.id || index}
                         card={card}
                         onClick={onCardClick}
                     />
+
                 );
 
             })}
 
         </div>
+
     );
+
 }
+
+
+const styles = {
+
+    hand: {
+
+        display: "flex",
+
+        flexDirection: "row",
+
+        flexWrap: "nowrap",
+
+        justifyContent: "center",
+
+        alignItems: "flex-end",
+
+        gap: "12px",
+
+        width: "100%",
+
+        minHeight: "230px",
+
+        padding: "15px 10px",
+
+        boxSizing: "border-box",
+
+        overflowX: "auto"
+
+    }
+
+};
