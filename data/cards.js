2a60b6267f9e42d99f8d836ea2795b128d9c098a
Иван <foxneversleep@gmail.com>
2026-08-14 21:50:52 +0300

    Update cards.js

--- data/cards.js
+++ data/cards.js
@@ -1,9 +1,45 @@
-{
-    id: "baba_yaga",
-    name: "Баба-Яга",
-    cost: 5,
-    attack: 6,
-    health: 7,
-    faction: "nav",
-    effects: [...]
-}
+```js
+const CARDS = {
+
+    baba_yaga: {
+        id: "baba_yaga",
+        name: "Баба-Яга",
+
+        type: "minion",
+        rarity: "SSR",
+        faction: "nav",
+
+        cost: 5,
+
+        attack: 6,
+        health: 7,
+
+        description: "Здесь пока будет описание способности.",
+
+        effects: []
+    },
+
+
+    shaman: {
+        id: "shaman",
+        name: "Шаман",
+
+        type: "minion",
+        rarity: "R",
+        faction: "magic",
+
+        cost: 3,
+
+        attack: 3,
+        health: 4,
+
+        description: "Здесь пока будет описание способности.",
+
+        effects: []
+    }
+
+};
+
+
+export default CARDS;
+```
