6779ae9976848312451dd779864660b01c975cf1
Иван <foxneversleep@gmail.com>
2026-08-18 21:59:50 +0300

    Update cards.js

--- data/cards.js
+++ data/cards.js
@@ -1,25 +1,111 @@
 const CARDS = [
-    {
-        id: "baba_yaga",
-        name: "Баба-Яга",
-        cost: 5,
-        attack: 4,
-        health: 6,
-        faction: "Навь",
-        image: "./assets/cards/baba_yaga.png",
-        description: "Хозяйка избушки на курьих ножках."
-    },
+{
+id: "baba_yaga",
+name: "Баба-Яга",
+rarity: "epic",
+faction: "Навь",
+cost: 6,
+health: 3500,
+attack: 1400,
+defense: 1050,
+strength: 12,
+image: "./assets/cards/epic/baba_yaga.png",
+abilities: [
+"Глаз ворона",
+"Контроль предков"
+],
+description: "Хозяйка избушки на курьих ножках."
+},
 
-    {
-        id: "shaman",
-        name: "Шаман",
-        cost: 3,
-        attack: 2,
-        health: 4,
-        faction: "Явь",
-        image: "./assets/cards/shaman.png",
-        description: "Знающий тайны духов и леса."
-    }
+{
+{
+    id: "svetogor",
+    name: "Светогор",
+    rarity: "epic",
+    faction: "Правь",
+    cost: 8,
+    health: 4800,
+    attack: 1800,
+    defense: 1400,
+    strength: 16,
+    image: "./assets/cards/epic/svetogor.png",
+    abilities: [],
+    description: "Древний богатырь исполинской силы."
+},
+
+
+{
+    id: "viy",
+    name: "Вий",
+    rarity: "epic",
+    faction: "Навь",
+    cost: 6,
+    health: 3100,
+    attack: 1250,
+    defense: 980,
+    strength: 11,
+    image: "./assets/cards/epic/viy.png",
+    abilities: [
+        "Взгляд ужаса",
+        "Снятие печатей"
+    ],
+    description: "Древнее чудовище, чей взгляд способен повергнуть врага в ужас."
+},
+
+
+{
+    id: "voin_pikhotinets",
+    name: "Пехотинец",
+    rarity: "common",
+    faction: "Явь",
+    cost: 1,
+    health: 90,
+    attack: 20,
+    defense: 40,
+    strength: 1,
+    image: "./assets/cards/common/voin_pikhotinets.png",
+    abilities: [
+        "Стройность",
+        "Стойкость"
+    ],
+    description: "Обычный воин, полагающийся на строй и дисциплину."
+},
+
+
+{
+    id: "volk",
+    name: "Волк",
+    rarity: "common",
+    faction: "Навь",
+    cost: 3,
+    health: 600,
+    attack: 250,
+    defense: 150,
+    strength: 8,
+    image: "./assets/cards/common/volk.png",
+    abilities: [],
+    description: "Дикий хищник, быстрый и опасный в ближнем бою."
+},
+
+
+{
+    id: "zmey_gorynych",
+    name: "Змей Горыныч",
+    rarity: "epic",
+    faction: "Навь",
+    cost: 7,
+    health: 3800,
+    attack: 1500,
+    defense: 1100,
+    strength: 14,
+    image: "./assets/cards/epic/zmey_gorynych.png",
+    abilities: [
+        "Огненное дыхание",
+        "Хвостовой удар"
+    ],
+    description: "Трёхглавое чудовище, способное обрушить на врага огонь и сокрушительный удар."
+}
+
 ];
 
 window.CARDS = CARDS;
