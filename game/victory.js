182342bbaa41e587748c1957923e2399400c1577
Иван <foxneversleep@gmail.com>
2026-08-19 12:42:21 +0300

    Create victory.js

---
+++ game/victory.js
@@ -0,0 +1,75 @@
+/*
+    ============================
+    VICTORY.JS
+
+    Победа / поражение
+    ============================
+*/
+
+
+
+function checkGameOver(state){
+
+
+
+    if(
+        state.player.hp <= 0
+    ){
+
+
+        return {
+
+
+            ...state,
+
+
+            gameOver:true,
+
+
+            winner:"opponent"
+
+
+        };
+
+
+    }
+
+
+
+
+    if(
+        state.opponent.hp <= 0
+    ){
+
+
+        return {
+
+
+            ...state,
+
+
+            gameOver:true,
+
+
+            winner:"player"
+
+
+        };
+
+
+    }
+
+
+
+
+    return state;
+
+
+}
+
+
+
+
+
+window.checkGameOver =
+checkGameOver;
