dc90e87eb78ef6d9974bf49f86c8c154c93a8184
Иван <foxneversleep@gmail.com>
2026-08-19 22:56:44 +0300

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,27 +1,84 @@
-function Card({ 
-    card, 
+/*
+============================
+
+Card.jsx
+
+Игровая карта
+
+Отвечает за:
+- отображение карты
+- стоимость
+- название
+- изображение
+- характеристики
+- редкость
+
+Логики игры здесь нет
+
+============================
+*/
+
+
+function Card({
+
+    card,
+
     onClick,
+
     mode="hand"
+
+
 }) {
 
 
-    if (!card) {
+
+    if(!card){
+
         return null;
+
     }
+
+
 
 
 
     const className =
 
+
         mode === "board"
+
 
         ?
 
+
         "card card-board"
+
 
         :
 
+
         "card card-hand";
+
+
+
+
+
+
+
+    const rarity =
+
+
+        card.rarity
+
+        ?
+
+        `rarity-${card.rarity}`
+
+        :
+
+        "";
+
+
 
 
 
@@ -30,91 +87,225 @@ function Card({
     return (
 
 
+
         <div
 
-            className={className}
 
-            onClick={() =>
-                onClick &&
-                onClick(card)
+
+            className={
+
+                `${className} ${rarity}`
+
             }
+
+
+
+            onClick={()=>{
+
+
+                if(onClick){
+
+                    onClick(card);
+
+                }
+
+
+            }}
+
+
 
         >
 
 
 
+
+
+
+
+            {/* СТОИМОСТЬ */}
+
+
             <div className="card-cost">
 
-                {card.cost}
+
+                {card.cost || 0}
+
 
             </div>
 
 
+
+
+
+
+
+
+
+            {/* НАЗВАНИЕ */}
 
 
             <div className="card-name">
 
+
                 {card.name}
+
+
 
             </div>
 
 
 
+
+
+
+
+
+
+            {/* АРТ */}
 
 
             <div className="card-image">
 
 
+
                 {
+
                     card.image
+
 
                     ?
 
+
                     <img
+
 
                         src={card.image}
 
-                        alt=""
+
+                        alt={card.name || ""}
+
 
                     />
 
+
                     :
 
+
                     <span>
+
                         АРТ
+
                     </span>
 
+
                 }
+
 
 
             </div>
 
 
+
+
+
+
+
+
+
+            {/* ХАРАКТЕРИСТИКИ */}
 
 
 
             <div className="card-stats">
 
 
+
                 <span>
-                    ⚔️ {card.attack}
+
+
+                    ⚔️
+
+                    {
+
+                        card.attack || 0
+
+                    }
+
+
                 </span>
 
 
+
+
+
+
+
                 <span>
-                    ❤️ {card.health}
+
+
+                    ❤️
+
+                    {
+
+                        card.health || 0
+
+                    }
+
+
                 </span>
+
 
 
             </div>
 
 
+
+
+
+
+
+
+
+            {/* ФРАКЦИЯ */}
+
+
+
+            {
+
+                card.faction &&
+
+
+
+                <div className="card-faction">
+
+
+                    {card.faction}
+
+
+
+                </div>
+
+
+            }
+
+
+
+
+
+
+
         </div>
+
 
 
     );
 
+
 }
+
+
+
+
 
 
 
