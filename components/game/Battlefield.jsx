16df79bda83bf5c951c6444c36cfb83386e5ccf8
Иван <foxneversleep@gmail.com>
2026-08-19 20:53:43 +0300

    Create Battlefield.jsx

---
+++ components/game/Battlefield.jsx
@@ -0,0 +1,142 @@
+/*
+============================
+Battlefield.jsx
+
+Игровое поле
+
+Отвечает за:
+- противника
+- игрока
+- поле существ
+- героев
+- руки
+
+Логики игры здесь нет
+
+============================
+*/
+
+
+function Battlefield({
+
+
+    player,
+
+    opponent,
+
+    handCards,
+
+    opponentHandCards,
+
+    selectedAttacker,
+
+    onCardClick,
+
+    onPlayerUnitClick,
+
+    onOpponentUnitClick,
+
+    onOpponentHeroClick
+
+
+}){
+
+
+    return (
+
+
+        <div>
+
+
+            <OpponentArea
+
+
+                opponent={opponent}
+
+
+                opponentHandCards={opponentHandCards}
+
+
+                selectedAttacker={selectedAttacker}
+
+
+                onOpponentUnitClick={onOpponentUnitClick}
+
+
+                onOpponentHeroClick={onOpponentHeroClick}
+
+
+            />
+
+
+
+
+
+
+
+            <GameTurn
+
+
+                selectedAttacker={selectedAttacker}
+
+
+                turn={window.currentTurn}
+
+            />
+
+
+
+
+
+
+
+
+
+            <PlayerArea
+
+
+                player={player}
+
+
+                selectedAttacker={selectedAttacker}
+
+
+                onPlayerUnitClick={onPlayerUnitClick}
+
+
+            />
+
+
+
+
+
+
+
+
+
+            <PlayerHand
+
+
+                cards={handCards}
+
+
+                onCardClick={onCardClick}
+
+
+            />
+
+
+
+        </div>
+
+
+    );
+
+
+}
+
+
+
+
+
+window.Battlefield = Battlefield;
