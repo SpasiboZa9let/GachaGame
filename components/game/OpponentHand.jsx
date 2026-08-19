edf11d3fb818aa629bf8c113d4ea2e3e2cdc8123
Иван <foxneversleep@gmail.com>
2026-08-19 19:49:14 +0300

    Create OpponentHand.jsx

---
+++ components/game/OpponentHand.jsx
@@ -0,0 +1,42 @@
+/*
+============================
+OpponentHand.jsx
+
+Рука противника
+
+Показывает карты противника.
+
+Логика игры отсутствует.
+
+============================
+*/
+
+
+function OpponentHand({
+
+
+    cards
+
+
+}){
+
+
+    return (
+
+
+        <Hand
+
+            cards={cards}
+
+        />
+
+
+    );
+
+
+}
+
+
+
+
+window.OpponentHand = OpponentHand;
