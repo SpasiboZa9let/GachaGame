9ff7d46996dc79de8470d32848acdea49382d70f
Иван <foxneversleep@gmail.com>
2026-08-19 09:30:39 +0300

    Update legendary.js

--- data/cards/legendary.js
+++ data/cards/legendary.js
@@ -1,223 +1,87 @@
 const LEGENDARY_CARDS = [
 
-    {
-        id: "yollupukki",
+{
+    id:"yollupukki",
 
-        name: "Йоллупукки",
+    name:"Йоллупукки",
 
-        rarity: "legendary",
+    rarity:"legendary",
 
-        cost: 8,
+    type:"creature",
 
-        health: 8500,
+    cost:10,
 
-        attack: 2800,
+    health:8500,
 
-        defense: 2500,
+    attack:2800,
 
-        strength: 8,
+    defense:2500,
 
-        faction: "prav",
+    strength:8,
 
-        abilities: [
-            "yollupukki_gift"
-        ],
+    faction:"prav",
 
-        description:
-            "Рождественский козёл. Может даровать союзникам подарки.",
+    image:
+    "./assets/cards/legendary/yollupukki.png",
 
-        image:
-            "./assets/cards/yollupukki.png"
-    },
+    abilities:[
 
+        {
+            id:"gift_yollupukki",
 
-    {
-        id: "zarnian",
+            name:"Подарок Йоллупукки",
 
-        name: "Зарниань",
+            type:"buff"
 
-        rarity: "legendary",
+        }
 
-        cost: 7,
+    ]
 
-        health: 4500,
+},
 
-        attack: 1800,
 
-        defense: 2200,
 
-        strength: 60,
+{
+    id:"zarnian",
 
-        faction: "prav",
+    name:"Зарниань",
 
-        abilities: [
-            "mother_blessing"
-        ],
+    rarity:"legendary",
 
-        description:
-            "Золотая Мать. Усиливается рядом с богами.",
+    type:"creature",
 
-        image:
-            "./assets/cards/zarnian.png"
-    },
+    cost:9,
 
+    health:4500,
 
-    {
-        id: "chernobog",
+    attack:1800,
 
-        name: "Чернобог",
+    defense:2200,
 
-        rarity: "legendary",
+    strength:60,
 
-        cost: 8,
+    faction:"prav",
 
-        health: 4800,
+    image:
+    "./assets/cards/legendary/zarnian.png",
 
-        attack: 1800,
+    abilities:[
 
-        defense: 1400,
+        {
+            id:"mother_blessing",
 
-        strength: 16,
+            name:"Материнское благословение",
 
-        faction: "nav",
+            type:"heal"
 
-        abilities: [
-            "chaos_field"
-        ],
+        }
 
-        description:
-            "Повелитель тьмы. Его присутствие искажает поле.",
+    ]
 
-        image:
-            "./assets/cards/chernobog.png"
-    },
-
-
-    {
-        id: "leshy",
-
-        name: "Леший",
-
-        rarity: "legendary",
-
-        cost: 6,
-
-        health: 4000,
-
-        attack: 1600,
-
-        defense: 1200,
-
-        strength: 15,
-
-        faction: "forest",
-
-        abilities: [
-            "forest_roots"
-        ],
-
-        description:
-            "Хозяин леса, сковывающий врагов корнями.",
-
-        image:
-            "./assets/cards/leshy.png"
-    },
-
-
-    {
-        id: "ilya_muromets",
-
-        name: "Илья Муромец",
-
-        rarity: "legendary",
-
-        cost: 8,
-
-        health: 4800,
-
-        attack: 1800,
-
-        defense: 1400,
-
-        strength: 16,
-
-        faction: "yav",
-
-        abilities: [
-            "damage_growth"
-        ],
-
-        description:
-            "Богатырь, становящийся сильнее от полученных ударов.",
-
-        image:
-            "./assets/cards/ilya_muromets.png"
-    },
-
-
-    {
-        id: "mara",
-
-        name: "Мара",
-
-        rarity: "legendary",
-
-        cost: 7,
-
-        health: 4500,
-
-        attack: 1700,
-
-        defense: 1350,
-
-        strength: 15,
-
-        faction: "nav",
-
-        abilities: [
-            "shadow_canvas"
-        ],
-
-        description:
-            "Хозяйка кошмаров и хаоса.",
-
-        image:
-            "./assets/cards/mara.png"
-    },
-
-
-    {
-        id: "svarog",
-
-        name: "Сварог",
-
-        rarity: "legendary",
-
-        cost: 8,
-
-        health: 4200,
-
-        attack: 1600,
-
-        defense: 1300,
-
-        strength: 14,
-
-        faction: "prav",
-
-        abilities: [
-            "svarog_flame"
-        ],
-
-        description:
-            "Бог небесного огня и кузнечного пламени.",
-
-        image:
-            "./assets/cards/svarog.png"
-    }
+}
 
 ];
 
 
 window.LEGENDARY_CARDS =
-    LEGENDARY_CARDS;
+LEGENDARY_CARDS;
